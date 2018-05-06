033de70259f21fa92c242bd4b7087907e2845948
William Schmitt <william@williamschmitt.org>
2018-05-06 16:51:28 +0200

    chore: new config structure

---
+++ bingo/config.ts
@@ -0,0 +1,5 @@
+export interface Config {
+  PATH: string,
+  cardCount: number,
+  alwaysTickedCenter: boolean
+}
--- bingo/lib/bingo.ts
+++ bingo/lib/bingo.ts
@@ -1,30 +1,98 @@
-const Config = require('../settings.ts');
+import { Config } from "../config";
 
-class Bingo {
-  words: Array<string>;
-  wordsStates: Array<boolean>;
+const Config: Config = require('../config.json');
+const fs = require('fs');
+const path = require('path');
 
-  constructor(words: Array<string>, state: Array<boolean>) {
-    if (words.length !== Config.wordsPerCard) {
-      if (words.length < Config.wordsPerCard) {
+export module Bingo {
+  export class Bingo {
+    tiles: Array<BingoTile>;
+
+    constructor(words: Array<string>, state?: Array<boolean>) {
+      if (words.length < Config.cardCount) {
         throw new Error(`Too little words in the provided array.`);
       }
-      this.words = []
-      while (this.words.length !== Config.wordsPerCard) {
-        const i = Math.floor((Math.random() * words.length));
-        this.words.push(words.splice(i)[0]);
+      if (!words) {
+        const wordsFilePath = path.resolve('../', Config.PATH);
+        const buf = fs.readFile(wordsFilePath);
+        const wds = JSON.parse(buf);
+        return new Bingo(buf);
+      } else if (!state) {
+        this.tiles = [];
+        for (let i = 0; i < Config.cardCount; i++) {
+          if (Config.alwaysTickedCenter && (i === Math.floor(Config.cardCount/2))) {
+            this.tiles.push(new BingoTile(null, true));
+          } else {
+            const j = Math.floor((Math.random() * words.length));
+            const word = words.splice(j)[0];
+            this.tiles.push(new BingoTile(word, false));
+          }
+        }
+      } else {
+        this.tiles = [];
+        for (let i = 0; i < words.length && i < state.length; i++) {
+          this.tiles.push(new BingoTile(words[i], state[i]));
+        }
       }
-
-      this.wordsStates = [];
-      for (let i = 0; i < Config.wordsPerCard; i++) {
-        this.wordsStates.push(false);
-      }
-    } else {
-      this.words = words;
-      this.wordsStates = state;
     }
 
+    updateTickState(index:number): void;
+    updateTickState(word: string): void;
+
+    updateTickState(arg: any) {
+      if (typeof arg === 'number') {
+        this.tiles[arg].tick();
+      } else if (typeof arg === 'string') {
+        this.tiles.map((word) => {
+          if (word.getWord() === arg) {
+            word.tick();
+          }
+        });
+      } else {
+        throw new TypeError('Invalid argument type for updateTickState.');
+      }
+    }
+
+    hasWinningLine(): boolean {
+      let columnsToExplore: Array<number> = [];
+      let winnableRow = true;
+      for (let i = 0; i < Math.sqrt(Config.cardCount); i++) {
+        columnsToExplore.push(i);
+      }
+      for (let i = 0; i < this.tiles.length; i++) {
+        if (i % Math.sqrt(Config.cardCount) === 0) {
+          // TODO: Assert previous row as won/lost
+          winnableRow = true;
+        }
+        winnableRow = winnableRow && this.tiles[i].ticked;
+
+        if (!this.tiles[i].ticked) {
+          const colValue = i % Math.sqrt(Config.cardCount);
+          columnsToExplore.findIndex((x) => x === colValue)
+        }
+
+        // TODO: fast-forward to next row if relevant
+      }
+      return false;
+    }
+  }
+
+  class BingoTile {
+    word: string;
+    ticked: boolean;
+
+    constructor(word: string, ticked?: boolean) {
+      this.word = word;
+      this.ticked = ticked || false;
+    }
+
+    tick() {
+      this.ticked = true;
+    }
+
+    getWord() {
+      return this.word;
+    }
   }
 }
-
 module.exports = Bingo;
